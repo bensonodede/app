e815ec9623ac514c68628df00760a60fe9738195
Benson Odede <Benson Odede>
2017-03-15 11:12:50 +0300

    fix

--- app.js
+++ app.js
@@ -74,32 +74,9 @@ io.on('connection', function (socket) {
 
 //socket to get recipient's form details
 io.on('connection', function(socket) {
-  socket.on('recipient', function (data) {
-    console.log(data.name);
-    console.log(data.address);
-    console.log(data.number);
-    app.post('/', function(req, res) {
-    var twilio = require('twilio');
-    var twiml = new twilio.TwimlResponse();
-    if (req.body.Body == 'hello') {
-      client.messages.create({
-        to: data.number,
-        from: "+16466797502",
-        body: "Your package has been picked up and is on the way.",
-      }, function(err, message) {
-        console.log(err);
-      })
-    } else if(req.body.Body == 'bye') {
-        twiml.message('Goodbye');
-    } else {
-        twiml.message('No Body param match, Twilio sends this in the request to your server.');
-    }
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
-});
 
 
-  });
+
   socket.on('sender', function (data) {
     console.log(data.name);
     console.log(data.number);
@@ -107,114 +84,55 @@ io.on('connection', function(socket) {
       coords: data.address
     }).then(function(response) {
       console.log(response);
-      client.messages.create({
+   client.messages.create({
           to: "+254724645546",
           from: "+16466797502",
-          body: "Pickup requested at " + "\n" + response + "\n" + "by " + data.name + "\n" + "Contact: " + data.number,
+          body: "PICKUP requested at " + "\n" + response + "\n" + "by " + data.name + "\n" + "Contact: " + data.number,
       }, function(err, message) {
          console.log(err);
       });
-
     });
-
-    //TODO: MIGHT HAVE TO SETUP TWILIO API INSIDE SCOKET
-    //TODO: DETERMINE WETHER 2 OR 3 TEXT MESSAGES ARE APPROPRIATE
-
-
-/*
-    app.post('/sms', function (req, res) {
-      var twilio = require('twilio');
-      var twiml = new twilio.TwimlResponse();
-      twiml.message('The Robots are coming! Head for the hills!');
-      res.writeHead(200, {'Content-Type': 'text/xml'});
-      res.end(twiml.toString());
-    });
-*/
+  });
 
 
 
+  socket.on('recipient', function (data) {
+    console.log(data.name);
+    console.log(data.address);
+    console.log(data.number);
+    client.messages.create({
+    to: "+254724645546",
+    from: "+16466797502",
+    body: "DROPOFF at " + "\n" + data.address + "\n" + "by " + data.name + "\n" + "Contact: " + data.number,
+  }, function(err, message) {
+  console.log(err);
+});
+
+app.post('/', function(req, res) {
+  var twiml = new twilio.TwimlResponse();
+  if (req.body.Body == Number ) {
+    var rec = String(Number);
+    client.messages.create({
+      to: rec,
+      from: "+16466797502",
+      body: "Your package has been picked up and is on the way.",
+    }, function(err, message) {
+      console.log(err);
+    })
+  } else if(req.body.Body == 'bye') {
+    twiml.message('Goodbye');
+  } else {
+    twiml.message('No Body param match, Twilio sends this in the request to your server.');
+  }
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
 
   });
 
 });
 
 
-
-
-
-
-//TODO: MIGHT HAVE TO SETUP TWILIO API INSIDE SOCKET
-//TODO: DETERMINE WETHER 2 OR 3 TEXT MESSAGES ARE APPROPRIATE
-/************************* TWILIO SMS SETUP **************************/
-/*
-//Twilio Credentials
-var accountSid = 'AC34811ee8499db4fbce70aa2a0bb150ec';
-var authToken = '75dd14e5a5669d3533652ab3a6d2f445';
-
-// Require  the twiio module and create a REST client
-var client = require('twilio')(accountSid, authToken);
-
-client.messages.create({
-    to: "+254724645546",
-    from: "+15017250604",
-    body: "This is the ship that made the Kessel Run in fourteen parsecs?",
-}, function(err, message) {
-//    console.log(message.sid);
-});
-
-
-app.post('/sms', function (req, res) {
-  var twilio = require('twilio');
-  var twiml = new twilio.TwimlResponse();
-  twiml.message('The Robots are coming! Head for the hills!');
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-});
-
-
-*/
-
-
-
-
-
-
-/*
-var database = firebase.database();
-
-function loadFirebase(){
-  var ref = database.ref("user");
-  ref.on("value", gotData, errData);
-}
-
-function errData(error) {
-  console.log("Something went wrong.");
-  console.log(error);
-}
-
-// The data comes back as an object
-function gotData(data) {
-  var user = data.val();
-  console.log(user);
-  // Grab all the keys to iterate over the object
-  var keys = Object.keys(user);
-}
-
-loadFirebase();*/
-
-
-
-
-/*/Write data to firebase database
-function writeData() {
-  var ref = database.ref('sender');
-
-  var data = {
-    location: response
-  }
-  ref.push(data);
-}
-writeData();*/
 
 
 
